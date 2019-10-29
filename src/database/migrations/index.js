83eb9bff8f5943762266c1147e4af5c54079432e
Darlan Costa <darlandsv@gmail.com>
2019-10-29 15:59:03 -0300

    2019-10-29

--- src/database/migrations/index.js
+++ src/database/migrations/index.js
@@ -1,12 +1,14 @@
 import Sequelize from 'sequelize';
-import User from '../app/models/User';
-import databaseConfig from '../config/database';
+import databaseConfig from '../../config/database';
 
 class Database {
   constructor() {
     this.init();
   }
+
   init() {
     this.connection = new Sequelize(databaseConfig);
   }
 }
+
+export default Database;
